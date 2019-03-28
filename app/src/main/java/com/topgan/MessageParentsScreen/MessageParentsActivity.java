5a5855c51b39f1df69fa34d6457419dba9db2015
snirozery <snir@prody.me>
2019-03-28 21:57:49 +0200

    pictures added to drawable and screen of reminder updated

--- app/src/main/java/com/topgan/MessageParentsScreen/MessageParentsActivity.java
+++ app/src/main/java/com/topgan/MessageParentsScreen/MessageParentsActivity.java
@@ -2,14 +2,34 @@ package com.topgan.MessageParentsScreen;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import com.topgan.R;
 
 public class MessageParentsActivity extends AppCompatActivity {
+
+    private RecyclerView recyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager layoutManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_message_parents);
+        recyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+
+
+        // use this setting to improve performance if you know that changes
+        // in content do not change the layout size of the RecyclerView
+        recyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        layoutManager = new LinearLayoutManager(this);
+        recyclerView.setLayoutManager(layoutManager);
+
+        // specify an adapter (see also next example)
+//        mAdapter = new ReminderAdapter(myDataset, context);
+        recyclerView.setAdapter(mAdapter);
     }
 }
---
+++ app/src/main/java/com/topgan/MessageParentsScreen/ReminderAdapter.java
@@ -0,0 +1,69 @@
+package com.topgan.MessageParentsScreen;
+
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.topgan.R;
+
+import java.util.List;
+
+public class ReminderAdapter extends RecyclerView.Adapter<ReminderAdapter.MyViewHolder> {
+    private List<Reminder> reminderList;
+    private Context context;
+
+    public static class MyViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public TextView tvTitle, tvReoccurance;
+        public ImageView ivPhotoId, ivChecked;
+
+        public MyViewHolder(View v) {
+            super(v);
+
+            tvTitle = itemView.findViewById(R.id.tvReminderTitle);
+            tvReoccurance = itemView.findViewById(R.id.tvReoccurance);
+            ivChecked = itemView.findViewById(R.id.ivChecked);
+            ivPhotoId = itemView.findViewById(R.id.ivPhotoId);
+        }
+    }
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public ReminderAdapter(List<Reminder> myDataset, Context context) {
+        reminderList = myDataset;
+        this.context = context;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public ReminderAdapter.MyViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.reminder_item, parent, false);
+
+        MyViewHolder vh = new MyViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(MyViewHolder holder, int position) {
+        Reminder item = reminderList.get(position);
+        if (item != null) {
+
+            holder.tvTitle.setText(item.reminderTitle);
+            holder.tvReoccurance.setText(item.reminderRecurrence);
+            holder.ivPhotoId.setImageResource(item.photoId);
+
+        }
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return reminderList != null ? reminderList.size() : 0;
+    }
+}
