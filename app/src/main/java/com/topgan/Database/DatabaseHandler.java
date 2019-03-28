3b21347f73e909b9d7636532a98c6242c86834b0
Roi Greenberg <roigreenberg@gmail.com>
2019-03-28 16:59:14 +0200

    add firestore + test

---
+++ app/src/main/java/com/topgan/Database/DatabaseHandler.java
@@ -0,0 +1,35 @@
+package com.topgan.Database;
+
+import android.support.annotation.NonNull;
+import android.util.Log;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+public class DatabaseHandler {
+    private static final String TAG = "DatabaseHandler";
+
+    public static void getTest() {
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        DocumentReference docRef = db.collection("test").document("IvJoOh8FEGGLvKu6OScw");
+        docRef.get().addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+            @Override
+            public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                if (task.isSuccessful()) {
+                    DocumentSnapshot document = task.getResult();
+                    if (document.exists()) {
+                        Log.d(TAG, "DocumentSnapshot data: " + document.getData());
+                    } else {
+                        Log.d(TAG, "No such document");
+                    }
+                } else {
+                    Log.d(TAG, "get failed with ", task.getException());
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/topgan/MainScreenActivity.java
+++ app/src/main/java/com/topgan/MainScreenActivity.java
@@ -2,6 +2,7 @@ package com.topgan;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.topgan.Database.DatabaseHandler;
 
 public class MainScreenActivity extends AppCompatActivity {
 
@@ -9,5 +10,8 @@ public class MainScreenActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_screen);
+
+        DatabaseHandler.getTest();
+
     }
 }
